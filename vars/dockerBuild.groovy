05f434ba3d886c10cc013f06dbdf436be551b76e
kumarsuraj14253 <kumarsuraj14253@gmail.com>
2023-10-31 01:57:04 +0530

    docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,6 +2,6 @@ def call(String project,String imagetag, String hubUser){
     sh """
      docker build -t ${hubUser}:${project} .
      docker tag ${hubUser}/${project} ${hubUser}/${project}:${imagetag}
-     docker tag ${hubUser}/${project} ${hubUser}/${project}:${imagetag}:latest
+     docker tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
 }
