27f44ef11e0af4408ffcb4e0d47e13ad8727c843
kumarsuraj14253 <kumarsuraj14253@gmail.com>
2023-10-31 19:58:56 +0530

    updated login ecr

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,8 +1,7 @@
-def call(String project, String imagetag, String hubUser, String imagename){
+def call(String project, String imagetag, String hubUser){
     
     sh """
      docker rmi -f ${hubUser}/${project}:${imagetag}
      docker rmi -f ${hubUser}/${project} ${hubUser}/${project}:latest
-     docker rmi -f ${imagename}
     """
      }
