7f17b57bbe6f7f644ffe365d829848b440b3b74c
kumarsuraj14253 <kumarsuraj14253@gmail.com>
2023-10-31 17:46:34 +0530

    updated login ecr

--- vars/dockerLoginECR.groovy
+++ vars/dockerLoginECR.groovy
@@ -1,3 +1,5 @@
 def call(String aws_account_id, String awsRegion, String ecrRepoName){
-    sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${awsRegion}.amazonaws.com"
+    sh """
+     aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${awsRegion}.amazonaws.com
+    """
 }
