789fe5cb12cca3ca39007230853d986d6e97962f
kumarsuraj14253 <kumarsuraj14253@gmail.com>
2023-10-31 19:37:52 +0530

    updated login ecr

--- vars/dockerLoginECR.groovy
+++ vars/dockerLoginECR.groovy
@@ -1,6 +1,10 @@
 def call(String aws_account_id, String awsRegion){
 
-     withCredentials([<object of type com.cloudbees.jenkins.plugins.awscredentials.AmazonWebServicesCredentialsBinding>])
-      {sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${awsRegion}.amazonaws.com"}
+     withCredentials([[
+        $class: "AmazonWebServicesCredentialsBinding",
+        credentialsId: "818404112479",
+        accessKeyVariable: "AWS_ACCESS_KEY_ID",
+        secretKeyVariable: "AWS_SECRET_ACCESS_KEY"]])          
+            {sh "aws ecr get-login-password --region ${awsRegion} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${awsRegion}.amazonaws.com"}
 
 }
